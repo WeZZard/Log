3447a1de812bd5f630376467702ce4abfb9be2ea
WeZZard <me@wezzard.com>
2018-10-16 15:44:33 +0800

    Adds basic test cases.

--- LogTests/LogTests.swift
+++ LogTests/LogTests.swift
@@ -13,5 +13,19 @@ import Log
 
 
 class LogTests: XCTestCase {
+    func testLogWithSubsystemCategoryTypeDsoMessageArgs_doesNotThrow() {
+        XCTAssertNoThrow(Log.log(category: "Any", message: ""))
+    }
     
+    func testLogWithSubsystemClassTypeDsoMessageArgs_doesNotThrow() {
+        XCTAssertNoThrow(Log.log(class: NSObject.self, message: ""))
+    }
+    
+    func testLogWithSubsystemObjectTypeDsoMessageArgs_doesNotThrow() {
+        XCTAssertNoThrow(Log.log(object: NSObject(), message: ""))
+    }
+    
+    func testLogWithPresetBehaviorTypeDsoMessageArgs_doesNotThrow() {
+        XCTAssertNoThrow(Log.log(message: ""))
+    }
 }
